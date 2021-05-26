311de3c7c07509e7932ba982b9df8ba9aacdc1b6
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-05-26 22:38:07 +0500

    Внедрен макет для основного сайта.

---
+++ FactorialEK.AspNetCore/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FactorialEK.AspNetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("~/Account/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ FactorialEK.AspNetCore/Controllers/ShopController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FactorialEK.AspNetCore.Controllers
+{
+    public class ShopController : Controller
+    {
+        
+    }
+}
