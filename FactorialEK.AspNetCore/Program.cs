a8a2594937e4c73f647dcf7c0a7fd5ed07da2147
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-06-14 12:19:25 +0500

    Обновление.

--- FactorialEK.AspNetCore/Program.cs
+++ FactorialEK.AspNetCore/Program.cs
@@ -20,7 +20,8 @@ namespace FactorialEK.AspNetCore
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                              .UseUrls("https://localhost:5003/");
                 });
     }
 }
